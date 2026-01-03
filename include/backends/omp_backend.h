0c3bc9c4d5e3638a84cfee196ca859a72e1aab7c
Akhil Samavedam <akhil.samavedam@gmail.com>
2026-01-02 20:02:51 -0600

    Removed TID overhead in OMP loop

--- include/backends/omp_backend.h
+++ include/backends/omp_backend.h
@@ -3,26 +3,21 @@
 #include <omp.h>
 #include <cstdint>
 #include <mc_kernel.h>
-#include <bits/fs_fwd.h>
 
 namespace mc {
-    inline uint64_t thread_seed() {
-        return static_cast<uint64_t>(omp_get_thread_num());
-    }
-
     template <typename Problem>
     double run(const Problem& problem, const OMPBackend&) {
         double sum = 0.0;
+
         #pragma omp parallel reduction(+:sum)
         {
-            double local_sum = 0.0;
-            RNGState rng(0, thread_seed());
-            #pragma omp for schedule(static)
-            for (uint64_t i = 0; i < problem.n_paths; i++) {
-                rng.counter = i;
-                local_sum += problem.kernel(rng);
+            const uint64_t tid = omp_get_thread_num();
+
+            #pragma omp for simd schedule(static)
+            for (uint64_t i = 0; i < problem.n_paths; ++i) {
+                RNGState rng{i, tid};
+                sum += problem.kernel(rng);
             }
-            sum += local_sum;
         }
         return sum / problem.n_paths;
     }
